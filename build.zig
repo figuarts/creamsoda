4c35fb9943dac0e84dd2379ae9d20ca5c85835e6
thenandthan <191000184+thenandthan@users.noreply.github.com>
2024-12-07 23:43:04 -0800

    init

---
+++ build.zig
@@ -0,0 +1,30 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const exe = b.addExecutable(.{
+        .name = "creamsoda",
+        .target = target,
+        .optimize = optimize,
+    });
+
+    exe.linkLibC();
+    exe.addCSourceFiles(.{ .files = sources, .flags = &.{} });
+
+    b.installArtifact(exe);
+
+    const run_cmd = b.addRunArtifact(exe);
+
+    run_cmd.step.dependOn(b.getInstallStep());
+
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
+
+    const run_step = b.step("run", "Run the app");
+    run_step.dependOn(&run_cmd.step);
+}
+
+const windows_sources = [_][]const u8{"src/main.c"};
